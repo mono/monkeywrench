f6fd548482b151cf5029cf8c1305273d53fcf5c4
Rolf Bjarne Kvinge <RKvinge@novell.com>
2009-07-28 10:39:12 +0000

    2009-07-28  Rolf Bjarne Kvinge  <RKvinge@novell.com>
    	* ViewHtmlReport.aspx.cs: Optimize a bit by transmitting data as
    	we transform it, instead of using a temporary file and call
    	TransmitFile. This also fixes an issue with TransmitFile and apache.
    
    svn path=/trunk/monkeywrench/; revision=138820

--- MonkeyWrench.Web.UI/ViewHtmlReport.aspx.cs
+++ MonkeyWrench.Web.UI/ViewHtmlReport.aspx.cs
@@ -22,7 +22,7 @@ using MonkeyWrench.Web.WebServices;
 
 public partial class ViewHtmlReport : System.Web.UI.Page
 {
-	private static void FixupHtml (string filename, int workfile_id, string md5)
+	private static void FixupHtml (string filename, int workfile_id, string md5, TextWriter writer)
 	{
 		string [] find;
 		string [] replace;
@@ -42,29 +42,22 @@ public partial class ViewHtmlReport : System.Web.UI.Page
 				string.Format ("a href=\"ViewHtmlReport.aspx?{1}={0}&amp;filename=", value, name),
 				string.Format ("a href='ViewHtmlReport.aspx?{1}={0}&amp;filename=", value, name)};
 
-		using (FileStream fs_writer = new FileStream (filename + ".out", FileMode.Create, FileAccess.Write, FileShare.Read)) {
-			using (StreamWriter writer = new StreamWriter (fs_writer)) {
-				using (FileStream fs_reader = new FileStream (filename, FileMode.Open, FileAccess.Read, FileShare.Read)) {
-					using (StreamReader reader = new StreamReader (fs_reader)) {
-						while (null != (line = reader.ReadLine ())) {
-							for (int i = 0; i < find.Length; i++)
-								line = line.Replace (find [i], replace [i]);
+		using (FileStream fs_reader = new FileStream (filename, FileMode.Open, FileAccess.Read, FileShare.Read)) {
+			using (StreamReader reader = new StreamReader (fs_reader)) {
+				while (null != (line = reader.ReadLine ())) {
+					for (int i = 0; i < find.Length; i++)
+						line = line.Replace (find [i], replace [i]);
 
-							// undo any changes for relative links
-							line = line.Replace (string.Format ("ViewHtmlReport.aspx?workfile_id={0}&amp;filename=#", workfile_id), "#");
-							// undo any changes for javascript links
-							line = line.Replace (string.Format ("ViewHtmlReport.aspx?workfile_id={0}&amp;filename=javascript", workfile_id), "javascript");
+					// undo any changes for relative links
+					line = line.Replace (string.Format ("ViewHtmlReport.aspx?workfile_id={0}&amp;filename=#", workfile_id), "#");
+					// undo any changes for javascript links
+					line = line.Replace (string.Format ("ViewHtmlReport.aspx?workfile_id={0}&amp;filename=javascript", workfile_id), "javascript");
 
-							writer.Write (line);
-							writer.Write ('\n');
-						}
-					}
+					writer.Write (line);
+					writer.Write ('\n');
 				}
 			}
 		}
-
-		File.Delete (filename);
-		File.Move (filename + ".out", filename);
 	}
 
 	protected void Page_Load (object sender, EventArgs e)
@@ -91,10 +84,8 @@ public partial class ViewHtmlReport : System.Web.UI.Page
 					if (web.ResponseHeaders ["Content-Encoding"] == "gzip")
 						MonkeyWrench.FileUtilities.GZUncompress (tmp_html_filename);
 
-					FixupHtml (tmp_html_filename, workfile_id, md5);
+					FixupHtml (tmp_html_filename, workfile_id, md5, Response.Output);
 				}
-
-				Response.TransmitFile (tmp_html_filename);
 			} catch (HttpException ex) {
 				Logger.Log ("ViewHtmlReport: Exception while download html: {0} (redirected to login page)", ex.Message);
 				Response.Redirect ("Login.asxp");
