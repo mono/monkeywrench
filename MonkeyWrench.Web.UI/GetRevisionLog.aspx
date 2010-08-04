4f6c2895df721fb4a68308b782a1b7c271a8e72d
Rolf Bjarne Kvinge <RKvinge@novell.com>
2010-08-04 08:53:52 +0800

    Pretty-print git diffs

--- MonkeyWrench.Web.UI/GetRevisionLog.aspx
+++ MonkeyWrench.Web.UI/GetRevisionLog.aspx
@@ -1,7 +1,9 @@
-﻿<%@ Page MasterPageFile="~/Master.master" Language="C#" AutoEventWireup="true" Inherits="GetRevisionLog" Codebehind="GetRevisionLog.aspx.cs" %>
+﻿<%@ Page MasterPageFile="~/Master.master" Language="C#" AutoEventWireup="true" Inherits="GetRevisionLog" Codebehind="GetRevisionLog.aspx.cs" EnableViewState="false" %>
 
 <asp:Content ID="Content2" ContentPlaceHolderID="content" runat="Server">
-		<pre id="log" runat="server"></pre>
+		<div id="log" runat="server"></div>
 		<br />
-		<pre id="diff" runat="server"></pre>
+		<div id="diff" runat="server"></div>
+        <br />
+        <%=Request ["raw"] == "true" ? string.Format ("<a href='GetRevisionLog.aspx?id={0}'>View colorized data</a>", Request ["id"]) : string.Format ("<a href='GetRevisionLog.aspx?id={0}&amp;raw=true'>View raw data</a>", Request ["id"]) %>
 </asp:Content>
--- MonkeyWrench.Web.UI/GetRevisionLog.aspx.cs
+++ MonkeyWrench.Web.UI/GetRevisionLog.aspx.cs
@@ -33,18 +33,96 @@ public partial class GetRevisionLog : System.Web.UI.Page
 	protected void Page_Load (object sender, EventArgs e)
 	{
 		int? revision_id = Utils.TryParseInt32 (Request ["id"]);
+		bool raw = Request ["raw"] == "true";
+		string d, l;
 
 		if (revision_id != null) {
 			try {
-				log.InnerText = WebServices.DownloadString (WebServices.CreateWebServiceDownloadRevisionUrl (revision_id.Value, false, Master.WebServiceLogin));
+				l = WebServices.DownloadString (WebServices.CreateWebServiceDownloadRevisionUrl (revision_id.Value, false, Master.WebServiceLogin));
+				if (raw) {
+					log.InnerHtml = "<pre>" + HttpUtility.HtmlEncode (l) + "</pre>";
+				} else {
+					log.InnerHtml = "<div style='background-color: #ccccff; padding: 5px; margin-top: 5px; margin-bottom: 5px;'><pre style='border-width: 0px;'>" + HttpUtility.HtmlEncode (l) + "</pre></div>";
+				}
 			} catch (Exception ex) {
 				log.InnerText = "Exception while fetching log: " + ex.ToString ();
 			}
 			try {
-				diff.InnerText = WebServices.DownloadString (WebServices.CreateWebServiceDownloadRevisionUrl (revision_id.Value, true, Master.WebServiceLogin));
+				d = WebServices.DownloadString (WebServices.CreateWebServiceDownloadRevisionUrl (revision_id.Value, true, Master.WebServiceLogin));
+				if (raw) {
+					diff.InnerHtml = "<pre>" + HttpUtility.HtmlEncode (d) + "</pre>";
+				} else {
+					diff.InnerHtml = ParseDiff (d);
+				}
 			} catch (Exception ex) {
 				diff.InnerText = "Exception while fetching diff: " + ex.ToString ();
 			}
 		}
+	}
+
+	private string ParseDiff (string diff)
+	{
+		if (diff.StartsWith ("diff --git")) {
+			return ParseGitDiff (diff);
+		} else {
+			return "<pre>" + HttpUtility.HtmlEncode (diff) + "</pre>";
+		}
+	}
+
+	private string ParseGitDiff (string diff)
+	{
+		StringBuilder result = new StringBuilder (diff.Length);
+		string line;
+		int old_ln = 0, new_ln = 0;
+		int old_d = 0, new_d = 0;
+		bool started = false;
+		result.Append ("<div>");
+		using (StringReader reader = new StringReader (diff)) {
+			while ((line = reader.ReadLine ()) != null) {
+				if (line.StartsWith ("diff --git")) {
+					// New file
+					if (started)
+						result.AppendLine ("</table>");
+					started = true;
+					result.AppendLine ("<table class='diff_view_table'>");
+					result.AppendFormat ("<tr><td class='diff_view_header_td' colspan='3'>{0}</td></tr>\n", line.Substring (10).Trim ().Split (' ') [0]);
+				} else if (line.StartsWith ("index")) {
+					// Not sure what this is
+					result.AppendFormat ("<tr><td class='diff_view_line_number'><pre class='diff_view_pre'>&nbsp;</pre></td><td class='diff_view_line_number'><pre class='diff_view_pre'>&nbsp;</pre></td><td class='diff_view_index_td'><pre class='diff_view_pre'>{0}</pre></td></tr>\n", line);
+				} else if (line.StartsWith ("---") || line.StartsWith ("+++")) {
+					// Ignore this for now
+					// style = "background-color: white";
+					// result.AppendFormat ("<tr style='{1}'><td colspan='3'>{0}</td></tr>", line, style);
+				} else if (line.StartsWith ("@@")) {
+					// line numbers
+					string [] nl = line.Replace ("@@", "").Trim ().Split (' ');
+					old_ln = int.Parse (nl [0].Substring (1, nl [0].IndexOf (',') - 1));
+					new_ln = int.Parse (nl [1].Substring (1, nl [1].IndexOf (',') - 1));
+					result.AppendFormat ("<tr><td class='diff_view_line_number'><pre class='diff_view_pre'>&nbsp;</pre></td><td class='diff_view_line_number'><pre class='diff_view_pre'>&nbsp;</pre></td><td class='diff_view_at_td'><pre class='diff_view_pre'>{0}</pre></td></tr>\n", line);
+				} else {
+					string cl;
+					if (line.StartsWith ("-")) {
+						cl = "diff_view_removed_line";
+						old_d = 1;
+						new_d = 0;
+					} else if (line.StartsWith ("+")) {
+						cl = "diff_view_added_line";
+						old_d = 0;
+						new_d = 1;
+					} else {
+						cl = "diff_view_normal_line";
+						old_d = 1;
+						new_d = 1;
+					}
+					result.AppendFormat ("<tr><td class='diff_view_line_number'><pre class='diff_view_pre'>{2}</pre></td><td class='diff_view_line_number'><pre class='diff_view_pre'>{3}</pre></td><td class='{1}'><pre class='diff_view_pre'>{0}</pre></td></tr>\n",
+						HttpUtility.HtmlEncode (line), cl, old_d == 0 ? string.Empty : old_ln.ToString (), new_d == 0 ? string.Empty : new_ln.ToString ());
+					old_ln += old_d;
+					new_ln += new_d;
+				}
+			}
+		}
+		result.AppendLine ("</table>");
+		result.AppendLine ("</div>");
+		return result.ToString ();
 	}
 }
