148594d51dee6ea7f4079e55dfbad58984f81fb3
Rolf Bjarne Kvinge <RKvinge@novell.com>
2010-12-23 16:20:33 +0100

    web.sh: add support for more MONO_OPTIONS

--- scripts/web.sh
+++ scripts/web.sh
@@ -11,7 +11,7 @@ cd `dirname $0`
 make -C .. all
 ROOT=`readlink -f $PWD/..`
 
-MONO_OPTIONS=--debug xsp2 --port 8123 --root $ROOT --applications /WebServices:$ROOT/MonkeyWrench.Web.WebService/,/:$ROOT/MonkeyWrench.Web.UI --nonstop >> /tmp/xsp2.log 2>&1
+MONO_OPTIONS="--debug $MONO_OPTIONS" xsp2 --port 8123 --root $ROOT --applications /WebServices:$ROOT/MonkeyWrench.Web.WebService/,/:$ROOT/MonkeyWrench.Web.UI --nonstop >> /tmp/xsp2.log 2>&1
 
 popd
 
