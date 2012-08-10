41ba54d73bfc79a73682609e184787277adb7d88
Duncan Mak <duncan.mak@xamarin.com>
2012-08-10 12:20:51 -0400

    Fix the default URL in the sample MonkeyWrench.xml.

--- MonkeyWrench.Web.UI/EditHost.aspx.cs
+++ MonkeyWrench.Web.UI/EditHost.aspx.cs
@@ -3,7 +3,7 @@
  *
  * Authors:
  *   Rolf Bjarne Kvinge (RKvinge@novell.com)
- *   
+ *
  * Copyright 2009 Novell, Inc. (http://www.novell.com)
  *
  * See the LICENSE file included with the distribution for details.
@@ -179,7 +179,7 @@ public partial class EditHost : System.Web.UI.Page
 			string txt = string.Format (@"
 <MonkeyWrench Version='2'>
 	<Configuration>
-		<WebServiceUrl>http://{0}/WebServices/</WebServiceUrl>
+		<WebServiceUrl>https://{0}/Wrench/WebServices/</WebServiceUrl>
 		<WebServicePassword>{1}</WebServicePassword>
 		<Host>{2}</Host>
 		<DataDirectory>[FULL PATH TO HOME DIRECTORY, NOT ~]/moonbuilder/data</DataDirectory>
