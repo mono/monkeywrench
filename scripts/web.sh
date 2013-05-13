86ab54e17cadbe45b1233ea9dd6f41e8afd89053
Jérémie Laval <jeremie.laval@gmail.com>
2013-05-13 18:34:50 -0400

    [web] Also remove the ServiceStack frontend from running on the server

--- scripts/web.sh
+++ scripts/web.sh
@@ -12,7 +12,7 @@ make -C .. all
 #ROOT=`readlink -f $PWD/..`
 ROOT=$PWD/..
 
-MONO_OPTIONS="--debug $MONO_OPTIONS" xsp4 --port 8123 --root $ROOT --applications /WebServices:$ROOT/MonkeyWrench.Web.WebService/,/:$ROOT/MonkeyWrench.Web.UI,/Api:$ROOT/MonkeyWrench.Web.ServiceStack/ --nonstop >> /tmp/xsp2.log 2>&1
+MONO_OPTIONS="--debug $MONO_OPTIONS" xsp4 --port 8123 --root $ROOT --applications /WebServices:$ROOT/MonkeyWrench.Web.WebService/,/:$ROOT/MonkeyWrench.Web.UI --nonstop >> /tmp/xsp2.log 2>&1
 
 popd
 
