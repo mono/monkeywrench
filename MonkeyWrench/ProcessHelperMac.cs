6b6d013bd8da8583c5d7126be66b1b8af9038eb9
Alan McGovern <alan.mcgovern@gmail.com>
2016-03-01 11:50:12 +0000

    Print out the process name as well as the pid
    This will help us figure out which processes we're trying
    to get stacktraces for.

--- MonkeyWrench/ProcessHelperMac.cs
+++ MonkeyWrench/ProcessHelperMac.cs
@@ -29,7 +29,7 @@ namespace MonkeyWrench
 
 		internal static void RenderStackTraceWithGdb (int pid, SynchronizedStreamWriter log)
 		{
-			log.WriteLine (string.Format ("\n * Fetching stack trace for process {0} * \n", pid));
+			log.WriteLine (string.Format ("\n * Fetching stack trace for process {0} (name '{1}') * \n", pid, GetProcessName (pid)));
 
 			var template = Path.GetTempFileName ();
 			try {
