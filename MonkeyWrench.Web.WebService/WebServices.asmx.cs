ab6d4e6dcd0cfd0a3c8607687e6be220804dba7f
Tim Miller <innerlogic4321@gmail.com>
2016-05-23 09:32:39 -0400

    Fix WebServices LoginOpenId

--- MonkeyWrench.Web.WebService/WebServices.asmx.cs
+++ MonkeyWrench.Web.WebService/WebServices.asmx.cs
@@ -83,7 +83,7 @@ namespace MonkeyWrench.WebServices
 
 			using (DB db = new DB ()) {
 				VerifyUserInRole (db, login, Roles.Administrator);
-				DBLogin_Extensions.LoginOutsideProvider (db, response, email, ip4, new List<string>());
+				DBLogin_Extensions.Login (db, response, email, ip4, new List<string>());
 				return response;
 			}
 		}
