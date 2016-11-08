57d6d3a2f899b63fa14fe19ee80f7ba900c41946
Joseph Stroman <joewstroman@gmail.com>
2016-11-08 15:01:36 -0500

    Send 404, with message to use getmanifest.aspx

--- MonkeyWrench.Web.UI/GetLatest.aspx.cs
+++ MonkeyWrench.Web.UI/GetLatest.aspx.cs
@@ -28,82 +28,12 @@ namespace MonkeyWrench.Web.UI
 			base.OnLoad (e); 
 			webServiceLogin = Authentication.CreateLogin (Request);
 
-			var laneName = Request.QueryString ["laneName"];
-
-			// NOTE: This is deliberately left this way because this endpoint has been superceded by GetManifest/GetMetadata.
-			var baseURL = Request.QueryString ["url"] ?? "http://storage.bos.internalx.com";
-			var storagePref = Request.QueryString ["prefer"];
-			if (!string.IsNullOrEmpty(storagePref) && (storagePref.ToLower () == "azure")) {
-				baseURL = "https://bosstoragemirror.blob.core.windows.net";
-			}
-			var updateRequest = false;
-			var step =  10;
-			var limit =  200;
-
-			var revision = getLatestRevision (webServiceLogin, laneName, step, 0, limit);
-
-			Action handleGetLatest = () => {
-				Response.AppendHeader ("Access-Control-Allow-Origin", "*");
-				Response.AppendHeader ("Content-Type", "text/plain");
-
-				HttpWebResponse response = makeHttpRequest (getManifestUrl (baseURL, laneName, revision));
-				if (response.StatusCode != HttpStatusCode.OK) {
-					// Default to NAS
-					if (storagePref != "NAS") {
-						response = makeHttpRequest (getManifestUrl ("http://storage.bos.internalx.com", laneName, revision));
-						if (response.StatusCode != HttpStatusCode.OK) {
-							Response.Write ("Can't find manifest");
-							return;
-						}
-					}
-					Response.Write ("Can't find manifest");
-					return;
-				}
-				using (var reader = new StreamReader (response.GetResponseStream ())) {
-					Response.Write (reader.ReadToEnd ());
-				}
-			};
-
-			if (revision != "") {
-				handleGetLatest ();
-			} else {
-				Response.Write ("No Valid Revisions");
-			}
+			Response.AppendHeader("Access-Control-Allow-Origin", "*");
+			Response.AppendHeader("Content-Type", "text/plain");
+			Response.StatusCode = 404;
+			Response.Write("GetLatest is deprecated, please use http://wrench.internalx.com/Wrench/GetManifest.aspx or http://wrench.internalx.com/Wrench/GetMetadata.aspx");
 		}
 
-		HttpWebResponse makeHttpRequest (string url) {
-			HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create (url);
-			return (HttpWebResponse)request.GetResponse ();
-		}
-
-		string getManifestUrl (string host, string laneName, string revision) {
-			return String.Format ("{0}/{1}/{2}/{3}/manifest", host, laneName, revision.Substring (0, 2), revision);
-		}
-
-		string getLatestRevision (WebServiceLogin login, string laneName, int step, int offset, int limit){
-			var lane = Utils.LocalWebService.FindLane (login, null, laneName).lane;
-			var revisions = Utils.LocalWebService.GetRevisions (login, null, laneName, step, offset).Revisions;
-			var revisionWorks = revisions.Select (r => Utils.LocalWebService.GetRevisionWorkForLane (login, lane.id, r.id, -1).RevisionWork).ToList ();
-			var validRevisions = revisionWorks.Find (wl => validRevision (login, wl));
-
-			if (validRevisions != null) {
-				return getRevisionName (revisions, validRevisions.First ().revision_id);
-			} else if (offset < limit) {
-				return getLatestRevision (login, laneName, step, offset + step, limit);
-			} else {
-				return "";
-			}
-		}
-
-		string getRevisionName (List<DBRevision> revisions, int revision_id) {
-			return revisions.Find (r => r.id == revision_id).revision;
-		}
-
-		bool validRevision (WebServiceLogin login, List<DBRevisionWork> revisionWorkList) {
-			return revisionWorkList.Any (r => 
-				Utils.LocalWebService.GetViewLaneData (login, r.lane_id, "", r.host_id, "", r.revision_id, "").WorkViews.Any (w => 
-					w.command.Contains ("upload-to-storage") && w.State == DBState.Success));
-		}
 	}
 }
 
